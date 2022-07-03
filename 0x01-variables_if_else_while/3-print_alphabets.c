e3a46cb1aafc8147bf90a0920a02210dcace12e5
TimAbidoye <timilehinabidoye96@gmail.com>
2022-07-03 13:15:52 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	char a;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (a = 'z'; a >= 'a'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
