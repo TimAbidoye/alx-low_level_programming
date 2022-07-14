77a2ca5722bd8c4892455982ee4942356d029eea
TimAbidoye <timilehinabidoye96@gmail.com>
2022-07-14 13:40:55 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -21,3 +21,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /*MAIN_H*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
