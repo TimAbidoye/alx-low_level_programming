40659ea80be91cce03c9df91f563e63227d2c7fa
TimAbidoye <timilehinabidoye96@gmail.com>
2022-07-14 12:53:58 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -21,4 +21,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /*MAIN_H*/
-void print_buffer(char *b, int size);
+char *cap_string(char *);
