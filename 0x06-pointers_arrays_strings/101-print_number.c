4d623b5573d21c06ac20769f3b4cf6634b0a3d4a
TimAbidoye <timilehinabidoye96@gmail.com>
2022-07-14 15:27:04 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,69 +1,35 @@
 #include "main.h"
 
-
-
 void print_helper(unsigned int n);
 
-
-
 /**
- *
- *  * print_number - prints an integer
- *
- *   * @n: a number
- *
- *    */
-
+ * print_number - prints an integer
+ * @n: a number
+ */
 void print_number(int n)
-
 {
-
-		if (n < 0)
-
-				{
-
-							_putchar('-');
-
-									n = -n;
-
-										}
-
-			print_helper((unsigned int)n);
-
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	print_helper((unsigned int)n);
 }
 
-
-
 /**
- *
- *  * print_helper - prints an unsigned integer
- *
- *   * @n: an unsigned integer to print
- *
- *    */
-
-
+ * print_helper - prints an unsigned integer
+ * @n: an unsigned integer to print
+ */
 
 void print_helper(unsigned int n)
-
 {
-
-		if (n < 10)
-
-				{
-
-							_putchar(n + '0');
-
-								}
-
-			else
-
-					{
-
-								print_helper(n / 10);
-
-										_putchar((n % 10) + '0');
-
-											}
-
+	if (n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		print_helper(n / 10);
+		_putchar((n % 10) + '0');
+	}
 }
