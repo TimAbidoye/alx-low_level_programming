b0eb9fb4d62fd99ebda5bacb4f000a6418d63587
TimAbidoye <timilehinabidoye96@gmail.com>
2022-07-18 09:38:03 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,4 +13,4 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif /*MAIN_H*/
-char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
