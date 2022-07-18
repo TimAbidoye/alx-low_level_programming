15cfb6fd5b0b79e109f8779e1187add5ddf4c65a
TimAbidoye <timilehinabidoye96@gmail.com>
2022-07-18 09:28:11 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,4 +13,4 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif /*MAIN_H*/
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
